d20ece2b3869569555684aaa263e3e9246d2c8ab
Chris Lomont <chris@lomont.org>
2024-12-15 08:44:24 -0500

    add

--- AdventOfCode/2024/Day15.cs
+++ AdventOfCode/2024/Day15.cs
@@ -43,12 +43,11 @@ namespace Lomont.AdventOfCode._2024
                     seq += line;
             }
 
-            // start pos
+            // draw board, get start pos
             var h = gl.Count;
             var w = gl[0].Length;
             var g = new char[w, h];
             var pos = new vec2();
-            //int sx = -1, sy = -1;
             Apply(g, (i, j, c) =>
             {
                 c = gl[j][i];
@@ -58,26 +57,15 @@ namespace Lomont.AdventOfCode._2024
             });
 
             // moves
+            Dictionary<char, vec2> dirs = new Dictionary<char, vec2> {
+                ['^'] = new vec2(0, -1),
+                ['>'] = new vec2(+1, 0),
+                ['v'] = new vec2(0, +1),
+                ['<'] = new vec2(-1, 0), };
             foreach (var m in seq)
-            {
-                switch (m)
-                {
-                    case 'v':
-                        Move(new vec2(0, 1));
-                        break;
-                    case '>':
-                        Move(new vec2(1, 0));
-                        break;
-                    case '<':
-                        Move(new vec2(-1, 0));
-                        break;
-                    case '^':
-                        Move(new vec2(0, -1));
-                        break;
-                }
-            }
+                Move(dirs[m]);
 
-
+            // scoring
             int answer = 0;
             Apply(g, (i, j, c) =>
             {
@@ -113,11 +101,11 @@ namespace Lomont.AdventOfCode._2024
                     Set(pos,'@');
                 }
 
-                if (ch == 'O')
+                if (boxs.Contains(ch))
                 {
                     // try push
                     var txy = xy;
-                    while (Get(txy) == 'O')
+                    while (boxs.Contains(Get(txy)))
                         txy += dir;
 
                     if (Get(txy) != '#')
@@ -136,8 +124,8 @@ namespace Lomont.AdventOfCode._2024
             {
                 //      Dump(g,noComma:true);
                 ////      Console.WriteLine();
-             var xy = pos + del;
-             var ch = Get(xy);
+                var xy = pos + del;
+                var ch = Get(xy);
                 if (ch == '#') return;
                 if (ch == '.')
                 {
@@ -152,7 +140,7 @@ namespace Lomont.AdventOfCode._2024
                     var (dx, dy) = del;
 
                     // try push
-                    var txy = new vec2(xy.x,xy.y);
+                    var txy = new vec2(xy.x, xy.y);
                     if (dx != 0)
                     {
                         while ("[]".Contains(Get(txy)))
@@ -163,11 +151,12 @@ namespace Lomont.AdventOfCode._2024
                             // shift
                             while (txy != xy)
                             {
-                                Set(txy,Get(txy-del));
+                                Set(txy, Get(txy - del));
                                 txy -= del;
                             }
+
                             // last one
-                            Set(xy,'.');
+                            Set(xy, '.');
 
                             Set(pos, '.');
                             pos = xy;
@@ -177,9 +166,9 @@ namespace Lomont.AdventOfCode._2024
                     else
                     {
                         HashSet<(int x, int y)> moved = new();
-                        if (CanPush(x, y, dx, dy, false, moved))
+                        if (CanPush(xy,del,/*x, y, dx, dy, */false, moved))
                         {
-                            CanPush(x, y, dx, dy, true, moved);
+                            CanPush(xy,del,/*x, y, dx, dy, */true, moved);
                             Set(pos, '.');
                             pos = xy;
                             Set(pos, '@');
@@ -189,8 +178,11 @@ namespace Lomont.AdventOfCode._2024
             }
 
             // given sq with box piece bx,by, and move dx,dy, return true if can push it
-            bool CanPush(int bx, int by, int dx, int dy, bool doMove, HashSet<(int, int)> moved)
+            bool CanPush(vec2 bxy, vec2 dir, /*int bx, int by, int dx, int dy, */bool doMove, HashSet<(int, int)> moved)
             {
+                var (bx, by) = bxy;
+                var (dx, dy) = dir;
+
                 var (bx2, by2) = (bx, by);
                 var c = g[bx, by];
                 if (c == '#') return false;
@@ -208,8 +200,9 @@ namespace Lomont.AdventOfCode._2024
                 }
                 Trace.Assert(c2 == g[bx2, by2]);
 
-                var m1 = g[bx + dx, by + dy] == '.' || CanPush(bx + dx, by + dy, dx, dy, doMove, moved);
-                var m2 = g[bx2 + dx, by2 + dy] == '.' || CanPush(bx2 + dx, by2 + dy, dx, dy, doMove, moved);
+                var bxy2 = new vec2(bx2,by2);
+                var m1 = g[bx + dx, by + dy] == '.' || CanPush(/*bx + dx, by + dy, dx, dy, */bxy+dir,dir,doMove, moved);
+                var m2 = g[bx2 + dx, by2 + dy] == '.' || CanPush(/*bx2 + dx, by2 + dy, dx, dy,*/bxy2+dir,dir, doMove, moved);
 
 
                 if (doMove)
